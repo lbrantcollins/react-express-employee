ec1bbdc235c30ad4f9d40adff5b03afb8f86b333
Sebastian Portuondo <slportuondo@gmail.com>
2019-08-12 00:53:50 -0500

    Created addEmployee function to communicate with employee api

--- src/App.js
+++ src/App.js
@@ -12,13 +12,41 @@ class App extends React.Component {
    }
 
    // CREATE: Sebastian
-   addEmployee = (employee, e) => {
-      
+   addEmployee = async (employee, e) => {
+      e.preventDefault
+      console.log(employee, e, 'Inside of addEmployee');
+
+      try {
+        const createEmployee = await fetch('http://localhost9000/api/v1/employees', {
+            method: 'POST',
+            credentials: 'include',
+            body: JSON.stringify(employee),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+        })
+        console.log(createEmployee, '<<< fetched createEmployee');
+
+        if (createEmployee.status !== 200) {
+          throw Error('Resource not found...')
+        }
+
+        const createEmployeeResponse = createEmployee.json();
+
+        this.setState({
+          employees: [...this.state.employees, createEmployeeResponse.data]
+        })
+
+
+      } catch (err) {
+        console.log('Inside of addEmployee: ', err);
+        return err
+      }
    }
 
    // READ: Brant
    getEmployees = () => {
-      
+
    }
 
    render() {
